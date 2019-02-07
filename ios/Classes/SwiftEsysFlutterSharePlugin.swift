e3b61e62e9175c0f99f561c75903e7ee2e4dc5cb
Alex X <iteratorx@zoho.eu>
2019-02-07 17:28:37 +0200

    export file by name in iOS

--- ios/Classes/SwiftEsysFlutterSharePlugin.swift
+++ ios/Classes/SwiftEsysFlutterSharePlugin.swift
@@ -24,17 +24,32 @@ public class SwiftEsysFlutterSharePlugin: NSObject, FlutterPlugin {
         let argsMap = arguments as! NSDictionary
         let fileName:String = argsMap.value(forKey: "fileName") as! String
         
-        // no use in ios
-        //let title:String = argsMap.value(forKey: "title") as! String
+	//let fileManager = NSFileManager.defaultManager()
+	//let urls = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+	//let documentDirectory: NSURL = urls.first as? NSURL
+	//let finalDatabaseURL = documentDirectory.URLByAppendingPathComponent(fileName)
         
+        
+	
+	//let documentsPathURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first 
+	//let finalDatabaseURL = documentsPathURL.appendingPathComponent(fileName)
+
         // load the iage
-        let docsPath:String = NSSearchPathForDirectoriesInDomains(.cachesDirectory,.userDomainMask , true).first!;
+        //let docsPath:String = NSSearchPathForDirectoriesInDomains(.cachesDirectory,.userDomainMask , true).first!;
+        let docsPath:String = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask , true).first!;
         let imagePath = NSURL(fileURLWithPath: docsPath).appendingPathComponent(fileName)
         let imageData:NSData? = NSData(contentsOf: imagePath!)
-        let imageToShare:UIImage = UIImage(data: imageData! as Data)!
+        //let imageToShare:UIImage = UIImage(data: imageData! as Data)!
+
+	//NSLog("trying to share")
+	//NSLog(fileName)
+	//let fileURL = URL(fileURLWithPath: fileName)
+        //let fileURL = NSURLfileURL(withPath:fileName)
+	
+	//let documento = NSData(contentsOfFile: fileName)
         
         // set up activity view controller
-        let activityViewController:UIActivityViewController = UIActivityViewController(activityItems: [imageToShare], applicationActivities: nil)
+        let activityViewController:UIActivityViewController = UIActivityViewController(activityItems: [imageData], applicationActivities: nil)
         
         // present the view controller
         let controller = UIApplication.shared.keyWindow!.rootViewController as! FlutterViewController
